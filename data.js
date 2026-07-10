e3853cf3eb5548ea85773ff19d49e9849ce89a05
unknown <homestudiothrissur@gmail.com>
2026-07-10 12:29:42 +0530

    2nd kit

--- data.js
+++ data.js
@@ -94,70 +94,70 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "3-dining-01",
-      "name": "Dining 01",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-dining-02",
-      "name": "Dining 02",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-stair",
+      "id": "3-stair",
       "name": "Stair",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dining-1",
+      "name": "Dining 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining-2",
+      "name": "Dining 2",
       "levels": [
         {
           "tileSize": 256,
@@ -218,7 +218,38 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "7-m-bed-01",
+      "id": "7-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-m-bed-01",
       "name": "M Bed 01",
       "levels": [
         {
@@ -249,7 +280,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "8-m-bed-02",
+      "id": "9-m-bed-02",
       "name": "M Bed 02",
       "levels": [
         {
@@ -280,7 +311,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "9-bed-01",
+      "id": "10-bed-01",
       "name": "Bed 01",
       "levels": [
         {
@@ -312,13 +343,13 @@ var APP_DATA = {
           "yaw": 1.6682287783081664,
           "pitch": 0.5371415238500195,
           "rotation": 0,
-          "target": "10-bed-02"
+          "target": "11-bed-02"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-bed-02",
+      "id": "11-bed-02",
       "name": "Bed 02",
       "levels": [
         {
@@ -350,7 +381,7 @@ var APP_DATA = {
           "yaw": -0.05545753832995004,
           "pitch": 0.21818687871629905,
           "rotation": 0,
-          "target": "9-bed-01"
+          "target": "10-bed-01"
         }
       ],
       "infoHotspots": []
